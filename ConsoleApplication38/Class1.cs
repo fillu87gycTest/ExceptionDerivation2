2271c1324226bc2e31431baa06493e1c910bb6fa
fillu87gyc <wander.off65@gmail.com>
2017-01-01 13:31:09 +0900

    add

---
+++ ConsoleApplication38/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class ExcptA : Exception
+{
+	public ExcptA() : base() { }
+	public ExcptA(string message) : base(message) { }
+	public override string ToString()
+	{
+		return Message;
+	}
+}
+class ExcptB : ExcptA
+{
+	public ExcptB() : base() { }
+	public ExcptB(string message) : base(message) { }
+	public override string ToString()
+	{
+		return Message;
+	}
+}
