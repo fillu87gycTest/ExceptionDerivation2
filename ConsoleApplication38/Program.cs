bacdffc87d328783d7466ec13b8b220b5bce63d5
fillu87gyc <wander.off65@gmail.com>
2017-01-01 13:24:32 +0900

    完成

--- ConsoleApplication38/Program.cs
+++ ConsoleApplication38/Program.cs
@@ -1,15 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/*
+ * catch ExptA
+ * catch ExptB
+ * System.Exception: 種類 'System.Exception' の例外がスローされました。
+ * 場所 Program.Main() 場所 D:\Program\Cs\ConsoleApplication38\ConsoleApplication38\Program.cs:行 15
+ * 続行するには何かキーを押してください . . .
+*/
 
-namespace ConsoleApplication38
+using System;
+
+class Program
 {
-	class Program
+	static void Main()
 	{
-		static void Main(string[] args)
+		for (int i = 0; i < 3; i++)
 		{
+			try
+			{
+				switch (i)
+				{
+					case 0:	 throw new ExcptA("catch ExptA");
+					case 1:  throw new ExcptB("catch ExptB");
+					default: throw new Exception();
+				}
+			}
+			//ここから下のcatch文の順番は順不同ではない
+			catch (ExcptB exc)
+			{
+				Console.WriteLine(exc);
+			}
+			catch (ExcptA exc)
+			{
+				Console.WriteLine(exc);
+			}
+			catch (Exception exc)
+			{
+				Console.WriteLine(exc);
+			}
 		}
 	}
 }
